fb795c918decc8fdbf43cce476f701d497f9a93e
miaecle <zqwu@stanford.edu>
2018-10-20 16:41:17 -0700

    metrics added

--- run_model.py
+++ run_model.py
@@ -13,6 +13,7 @@ import pandas as pd
 from scripts.biLSTM import BiLSTM, MultiheadAttention
 from scripts.models import TestModel
 from scripts.trainer import Trainer
+from sklearn.metrics import precision_score, recall_score
 import os
 import pickle
 
@@ -24,6 +25,19 @@ class Config:
     
 opt=Config()
 
+def evaluate(inputs, preds, thr=0.005):
+  precisions = []
+  recalls = []
+  for inp, pred in zip(inputs, preds):
+    label = inp[1]
+    _label = label > thr
+    _pred = pred > thr
+    precisions.append(precision_score(_label.flatten(), _pred.flatten()))
+    recalls.append(recall_score(_label.flatten(), _pred.flatten()))
+  print("Precision %f" % np.mean(precisions))
+  print("Recall %f" % np.mean(recalls))
+  return precisions, recalls
+
 n_charges = 7
 
 df = pd.read_csv("./output.csv")
@@ -32,17 +46,17 @@ ys = []
 X_metas = []
 for line in np.array(df):
   """
-  ['acetyl',
- 'name',
- 'charge',
- 'precursor',
- 'mz',
- 'intensity',
- 'ion',
- 'position',
- 'neutral_loss',
- 'ion_charge',
- 'delta']
+0  ['acetyl',
+1 'name',
+2 'charge',
+3 'precursor',
+4 'mz',
+5 'intensity',
+6 'ion',
+7 'position',
+8 'neutral_loss',
+9 'ion_charge',
+10 'delta']
   """
   Xs.append(np.array(eval(line[1])))
   X_metas.append((line[0], line[2]))
@@ -52,10 +66,13 @@ for line in np.array(df):
   ions = np.array(eval(line[6])).astype(int)
   ion_charges = np.array(eval(line[9])).astype(int)
   positions = np.array(eval(line[7])).astype(int)
+  offsets = np.array(eval(line[10])).astype(float)
   assert np.min(positions) >= 1
   assert np.max(positions) <= seq_len
   y = np.zeros((seq_len - 1, 2*n_charges))
   for i in range(len(intensities)):
+    if np.abs(offsets[i]) > 0.1:
+      continue
     y[positions[i]-1, ions[i]*n_charges + ion_charges[i] - 1] += intensities[i]
   y = y/np.sum(y)
   ys.append(y)
@@ -82,3 +99,6 @@ for i in range(20):
   if not os.path.exists('./saved_models'):
     os.mkdir('./saved_models')
   trainer.save(os.path.join('./saved_models', 'model-%d.pth' % i))
+  evaluate(train_input, trainer.predict(train_input))
+  evaluate(test_input, trainer.predict(test_input))
+  
