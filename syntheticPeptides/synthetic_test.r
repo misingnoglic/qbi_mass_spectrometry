196ed22a7eddeb51ecc22b174768834ee763902b
danserie <seri0027@umn.edu>
2018-10-21 13:23:14 -0700

    code to extract synth from msp added

--- syntheticPeptides/synthetic_test.r
+++ syntheticPeptides/synthetic_test.r
@@ -37,6 +37,7 @@ ev$seq <- paste0(ev$Sequence, '/', ev$Charge)
 
 ## only use those that were not employed in training
 keep <- intersect(ev$seq, syn2$V2)
+write.table(keep, "D:\\hackathon\\synthetic.tryptic.peptides.txt", quote=F, row.names=F, col.names=F)
 
 evk <- ev[ev$seq %in% keep,c('Sequence', 'seq','Length','Charge','Retention.time', 'Mass','m.z')]
 evk$Sequence <- as.character(evk$Sequence)
@@ -79,5 +80,43 @@ for (i in 1:nrow(dec2)){
 
 evk$decoy <- 'original'
 dec1$decoy <- 'decoy'
+dec1$seq <- dec1$scramble
 oot <- rbind(evk, dec1[,names(evk)])
+names(oot)[1] <- "Original.Seq"
 write.table(oot, "D:\\hackathon\\synth.peptides.plus.decoys.csv", sep=',', row.names=F, col.names=T)
+
+
+## Read in Mike's MSP file of all spectra, subset to synthetic peptides.
+tot <- read.table("D:\\hackathon\\FTMS-HCD-28.msp", sep='%', stringsAsFactors=F, as.is=T) 
+
+tot$n <- 1:nrow(tot)
+
+tot$nam <- gsub("Name: ", '', tot$V1)
+
+tot2 <- tot[which(tot$nam %in% keep),]
+
+keepvec <- c(tot2$n[1]:(tot2$n[1]+60))
+for (i in 2:length(tot2$n)){
+  keepvec <- c(keepvec, c(tot2$n[i]:(tot2$n[i]+60)))
+}
+
+tot3 <- tot[keepvec,]
+tot3$seq <- NA
+#tot3 <- tot3[-grep('MW:', tot3$V1),]
+#tot3 <- tot3[-grep('Comment:', tot3$V1),]
+#tot3 <- tot3[-grep('Num', tot3$V1),]
+
+globvar <- NA
+for (i in 1:nrow(tot3)){
+  if (regexpr('Name: ', tot3[i,'V1']) > 0){
+    globvar <- tot3[i,'nam']
+  } 
+  tot3$seq[i] <- globvar
+}
+tot4 <- tot3[which(tot3$seq %in% keep),]
+
+
+totf <- tot[tot4$n,]
+write.table(totf[,1], "D:\\hackathon\\synthetic.peptides.msp", row.names=F, col.names=F, quote=F)
+
+
